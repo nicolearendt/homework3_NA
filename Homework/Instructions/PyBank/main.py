cd8792a71dcf4e61d49a5f02fba9678539729129
Nicole Arendt <nicole.arendt23@gmail.com>
2019-02-26 18:20:40 -0500

    pybank and pypoll main.py created

---
+++ Homework/Instructions/PyBank/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 26 18:18:40 2019
+
+@author: nicolearendt
+"""
+
---
+++ Homework/Instructions/PyPoll/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 26 18:17:42 2019
+
+@author: nicolearendt
+"""
+
