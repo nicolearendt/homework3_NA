f59cfef3f6aa18bf6995117d21fbbea884067070
Nicole Arendt <nicole.arendt23@gmail.com>
2019-03-02 01:08:44 -0500

    PyBank finished

--- Homework/Instructions/PyBank/main.py
+++ Homework/Instructions/PyBank/main.py
@@ -1,51 +1,80 @@
 import os
 import csv
 
-#path to budget data
 csvpath = os.path.join("Resources", "budget_data.csv")
 
 print()
 print("Financial Analysis")
 print("------------------------")
 
-#get the total number of months by counting each row (after the header)
-def getMonthTotal():
-    month = sum(1 for row in csvreader)
-    return month
+months = []
+profits = []
+change = []
 
-#get the net total amount of the profits/losses
-def getNetTotal():
-    total = 0
+def getMonths():
     for row in csvreader:
-        total += int(row[1])
-    return total
-    
-def getAvg():
-    profits= []
+        months.append(row[0])
+
+def getProfits():
     for row in csvreader:
         profits.append(int(row[1]))
-    change = []
+        
+def getChange():
     for i in range(len(profits) - 1):
         change.append(profits[i+1] - profits[i])
-    avg = sum(change)/len(change)
-    return avg
+    return (sum(change)/len(change))
 
-
+def getInc():
+    inc = 0
+    index = 0
+    for i in range(len(change)):
+        if change[i] > inc:
+            inc = change[i]
+            index = i
+    return index
+    
+def getDec():
+    dec = 0
+    index = 0
+    for i in range(len(change)):
+        if change[i] < dec:
+            dec = change[i]
+            index = i
+    return index
 
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
-    mtotal = getMonthTotal()
-    print(f"Total Months: {mtotal}")
+    getMonths()
     
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
-    ntotal = getNetTotal()
-    print(f"Total: ${ntotal}")
+    getProfits()
     
-with open(csvpath, newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    csv_header = next(csvreader)
-    avgChange = round(getAvg(),2)
-    print(f"Average Change: ${avgChange}")
+monthsTotal = len(months)
+print(f"Total Months: {monthsTotal}")
+
+nettotal = 0
+for i in range(len(profits)):
+    nettotal += profits[i]
+print(f"Total: ${nettotal}")
+
+avgChange = round(getChange(),2)
+print(f"Average Change: ${avgChange}")
+
+indexInc = getInc()
+print(f"Greatest Increase in Profits: {months[indexInc+1]} (${change[indexInc]})")
+
+indexDec = getDec()
+print(f"Greatest Increase in Profits: {months[indexDec+1]} (${change[indexDec]})")
+
+file = open("Financial Analysis.txt", "w")
+file.write("Financial Analysis\n")
+file.write("------------------------\n")
+file.write(f"Total Months: {monthsTotal}\n")
+file.write(f"Total: ${nettotal}\n")
+file.write(f"Average Change: ${avgChange}\n")
+file.write(f"Greatest Increase in Profits: {months[indexInc+1]} (${change[indexInc]})\n")
+file.write(f"Greatest Increase in Profits: {months[indexDec+1]} (${change[indexDec]})\n")
+file.close()
